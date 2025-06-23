76f40de9b1d6a6b2be25fce6de61b68bb0b450f8
Heberth Deza <heberth2788@gmail.com>
2025-06-23 09:51:08 -0500

    add Profile struct

---
+++ guadalupereporta/snippets/model/Profile.swift
@@ -0,0 +1,28 @@
+//
+//  Profile.swift
+//  guadalupereporta
+//
+//  Created by Heberth Deza on 23/06/25.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications: Bool = true
+    var seasonalPhoto: Season = Season.winter
+    var goalDate: Date = Date()
+    
+    // 'default' is a reserved keyword, however, when we need to use it as
+    // identifier, we need to enclose it with `` characters.
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+}
